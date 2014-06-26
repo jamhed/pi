d61583d9634972bba7911c4de9a243f2a8460a14
Roman Galeev <ip@ncom-ufa.ru>
2014-06-27 02:44:54 +0600

    base structure

---
+++ public/js/conf/requirejs.js
@@ -0,0 +1,21 @@
+define({
+   catchError: true,
+   paths: {
+      "lib/URI":        "lib/uri.js/src",
+      "lib/jquery":     "lib/jquery/dist/jquery.min",
+      "lib/jquery-ui":  "lib/jquery-ui/ui/minified/jquery-ui.min",
+      "lib/doT":        "lib/doT/doT",
+      "lib/bootstrap":  "lib/bootstrap/dist/js/bootstrap.min"
+   },
+   shim: {
+      "lib/jquery": {
+         exports: "jQuery"
+      },
+      "lib/jquery-ui": {
+         deps: ["lib/jquery"]
+      },
+      "lib/bootstrap": {
+         deps: ["lib/jquery"]
+      },
+   }
+});
