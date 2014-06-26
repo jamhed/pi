f73c3b358f913476fce07cf2bf072879306e0ce3
Roman Galeev <ip@ncom-ufa.ru>
2014-06-27 02:28:30 +0600

    bower

--- cs/public.cs
+++ cs/public.cs
@@ -1,8 +1,6 @@
 require ["conf/requirejs"], (Conf) ->
    require.config Conf
 
-   args = window.location.search
-
    require ["router", "lib/bootstrap"], (Router, Bootstrap) -> 
       $.ajaxPrefilter (options, originalOptions, jqXHR) ->
          return options
